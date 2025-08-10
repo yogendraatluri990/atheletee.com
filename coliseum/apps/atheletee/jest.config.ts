5a42add15c3b76a5b834593b7d02518105261a32
Yogendra Atluri <yogendra.atluri@wcgclinical.com>
2025-08-10 09:55:48 -0400

    created initial repo for react with nx

---
+++ coliseum/apps/atheletee/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: '@coliseum/atheletee',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: 'test-output/jest/coverage',
+};
